52757c8f66efe4e4685ce9ab8808d9730cb53680
BC-macair028 <bc-macair028@BC-macair028s-MacBook-Air.local>
2022-02-03 09:44:49 +0200

    updated filenames and debugged

---
+++ __tests__/phoneRegister.test.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const PhoneRegister = require('../phoneRegister');
+const phones = require('../phones.json');
+
+describe('Testing constructor', ()=>{
+    test('Test 1: object created with given data',()=>{
+        const register = new PhoneRegister(phones);
+        expect(register.phoneRegister).toEqual(phones);
+    });
+
+    test('Test 2: missing parameter throws an exception',()=>{
+        expect(() => new PhoneRegister()).toThrow('phone data missing');
+    });
+});
--- phoneRegistry.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = class PhoneRegistry {
-    constructor(data){
-        if(!data) throw new Error('phone data missing');
-        this.PhoneRegistry=data;
-    }
- }
